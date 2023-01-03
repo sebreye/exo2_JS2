249e0c65a1fc504bdd1fcb7ff639612b96f94e11
sebastian Reyes <sebreyes222gmail.com>
2023-01-03 11:52:59 +0100

    ajout fct3

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,31 @@
 let nbr = reversenbr(12345)
 console.log(nbr);
 
+divPar2(6)
+divPar2(7)
+
+logIn()
 
 
-
-
-
+// fonction créer
 
 function reversenbr(n) {
     return n.toString().split("").reverse().join("");
 }
 function divPar2(x) {
     switch (x % 2) {
-      case 0:
+    case 0:
         console.log(`Le num ${x} est divisible par 2  ${x}:2 = ${x / 2}`);
         break;
-      default:
+    default:
         console.log(`Ce nombre n'est pas divisible par 2`);
     }
 }
+function logIn() {
+    let password = prompt("Entrez votre mot de passe:");
+    while (password !== "mdp") {
+    password = prompt("Mot de passe incorrect. Veuillez réessayer:");
+    }
+    alert("Vous êtes connecté");
+}
+
